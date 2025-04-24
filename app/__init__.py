c3599a489106c4f23f8489a97ca0c579f5e9b157
Jack Cooper <jc3123@pm.me>
2025-04-24 11:18:22 +0200

    added css and converted templates to inheritance

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from app import views
 
 app = Flask(__name__)  
 
-from app import views
-
 app.config.from_object('config.DevelopmentConfig')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///yourdatabase.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
--- app/views.py
+++ app/views.py
@@ -1,4 +1,19 @@
 from app import app
+from flask import render_template
+
 @app.route('/')
 def index():
-    return 'Hello, Flask!'
+    return render_template('home.html', title='home page')
+
+@app.route('/msg')
+def msg():
+    return render_template('msgexample.html', title='Message Page', \
+        name='John Doe', messages=['Hello', 'Hi', 'Hey'])
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html', title='blog page')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='about page')
